ae85c57bd3005cba4297b428632aa2982b3502bf
srnazng <serenazeng@fb.com>
2022-06-13 17:14:36 -0700

    Add bottom navigator

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -1,34 +1,58 @@
 package com.example.instagram;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnLogout;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+
+    // define fragments
+    final Fragment homeFragment = HomeFragment.newInstance();
+    final Fragment newPostFragment = NewPostFragment.newInstance();
+    final Fragment settingsFragment = SettingsFragment.newInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnLogout = findViewById(R.id.btnLogin);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            Fragment selectedFragment = null;
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        selectedFragment = homeFragment;
+                        break;
+                    case R.id.action_post:
+                        selectedFragment = newPostFragment;
+                        break;
+                    case R.id.action_settings:
+                        selectedFragment = settingsFragment;
+                        break;
+                    default: break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.rlContainer, selectedFragment).commit();
+                return true;
+            }
+        });
     }
 
-    public void logout(View v){
-        ParseUser.logOut();
-        ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
-
-        if(currentUser == null){
-            Intent i = new Intent(this, LoginActivity.class);
-            startActivity(i);
-        }
-    }
 
 }
---
+++ app/src/main/java/com/example/instagram/NewPostFragment.java
@@ -0,0 +1,47 @@
+package com.example.instagram;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link NewPostFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NewPostFragment extends Fragment {
+
+    public NewPostFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment NewPostFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static NewPostFragment newInstance() {
+        NewPostFragment fragment = new NewPostFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_new_post, container, false);
+    }
+}
